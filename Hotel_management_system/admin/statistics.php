f7bacc290dc85c4051982d31a7f539036361872c
smmilk <juzchong@gmail.com>
2023-11-22 21:44:15 +0800

    both chart done

--- Hotel_management_system/admin/statistics.php
+++ Hotel_management_system/admin/statistics.php
@@ -1,11 +1,11 @@
 <?php
 $feedbackCursor = $feedbackCollection->find();
 
-// Initialize arrays to store room types and their average ratings
-$roomTypes = [];
+// Initialize arrays for bar chart
+$roomTypesBarChart = [];
 $averageRatings = [];
 
-// Process feedback data and calculate average ratings per room type
+// Process feedback data for bar chart
 foreach ($feedbackCursor as $feedback) {
     $bookingId = $feedback['feedback_id'];
     $rating = $feedback['rating'];
@@ -17,44 +17,68 @@ foreach ($feedbackCursor as $feedback) {
     $bookingResult = mysqli_query($con, $bookingQuery);
 
     if ($bookingResult->num_rows > 0) {
-        $row = $bookingResult->fetch_assoc();
+        $row = mysqli_fetch_assoc($bookingResult);
         $roomType = $row['type'];
 
         // Initialize arrays for room types if not already done
-        if (!isset($roomTypes[$roomType])) {
-            $roomTypes[$roomType] = [];
+        if (!isset($roomTypesBarChart[$roomType])) {
+            $roomTypesBarChart[$roomType] = [];
             $averageRatings[$roomType] = 0;
         }
 
         // Add rating to the array for the corresponding room type
-        $roomTypes[$roomType][] = $rating;
+        $roomTypesBarChart[$roomType][] = $rating;
     }
 }
 
 // Calculate average ratings for each room type
-foreach ($roomTypes as $roomType => $ratings) {
+foreach ($roomTypesBarChart as $roomType => $ratings) {
     $averageRatings[$roomType] = array_sum($ratings) / count($ratings);
 }
 
-// Convert data to JSON for use in JavaScript
-$roomTypesJson = json_encode(array_keys($roomTypes));
+// Convert bar chart data to JSON for use in JavaScript
+$roomTypesBarChartJson = json_encode(array_keys($roomTypesBarChart));
 $averageRatingsJson = json_encode(array_values($averageRatings));
 
+// Query MySQL to get booking data for line chart
+$bookingQueryLineChart = "SELECT MONTH(check_in_date) AS month, COUNT(*) AS num_bookings
+                          FROM booking
+                          GROUP BY month
+                          ORDER BY month";
+$bookingResultLineChart = mysqli_query($con, $bookingQueryLineChart);
+
+// Initialize arrays for line chart
+$months = [];
+$numBookings = [];
+
+// Process booking data for line chart
+while ($row = mysqli_fetch_assoc($bookingResultLineChart)) {
+    $months[] = date('F', mktime(0, 0, 0, $row['month'], 1));
+    $numBookings[] = $row['num_bookings'];
+}
+
+// Convert line chart data to JSON for use in JavaScript
+$monthsJson = json_encode($months);
+$numBookingsJson = json_encode($numBookings);
 ?>
 
 <!-- Create a canvas for the bar chart -->
-<canvas id="roomRatingsChart" width="400" height="200"></canvas>
+<canvas id="roomRatingsChart" width="400" height="200"></canvas><hr>
+
+<!-- Create a canvas for the line chart -->
+<canvas id="bookingLineChart" width="400" height="200"></canvas>
 
 <script>
     // Retrieve data from PHP and create a bar chart using Chart.js
-    var roomTypes = <?php echo $roomTypesJson; ?>;
+    var roomTypesBarChart = <?php echo $roomTypesBarChartJson; ?>;
     var averageRatings = <?php echo $averageRatingsJson; ?>;
+    var selectedRoomType = 'all';
 
-    var ctx = document.getElementById('roomRatingsChart').getContext('2d');
-    var chart = new Chart(ctx, {
+    var ctxBarChart = document.getElementById('roomRatingsChart').getContext('2d');
+    var barChart = new Chart(ctxBarChart, {
         type: 'bar',
         data: {
-            labels: roomTypes,
+            labels: roomTypesBarChart,
             datasets: [{
                 label: 'Average Rating',
                 data: averageRatings,
@@ -74,6 +98,41 @@ $averageRatingsJson = json_encode(array_values($averageRatings));
                 title: {
                     display: true,
                     text: 'Average Ratings by Room Type',
+                    font: {
+                        size: 16
+                    }
+                }
+            }
+        }
+    });
+
+    // Retrieve data from PHP and create a line chart using Chart.js
+    var months = <?php echo $monthsJson; ?>;
+    var numBookings = <?php echo $numBookingsJson; ?>;
+
+    var ctxLineChart = document.getElementById('bookingLineChart').getContext('2d');
+    var lineChart = new Chart(ctxLineChart, {
+        type: 'line',
+        data: {
+            labels: months,
+            datasets: [{
+                label: 'Number of Bookings',
+                data: numBookings,
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 2,
+                fill: false
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Total Bookings per Month',
                     font: {
                         size: 16
                     }
